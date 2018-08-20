b10f08e90bde6aaca2bd2aa89d8d2428fcb01b9b
Stéphane Lüthi <stephane.luethi@teezy.co>
2018-08-20 10:24:33 +0000

    Setup email functionality for sing.up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'https://7e9a0c593e0c410899352235ee26b660.vfs.cloud9.us-east-2.amazonaws.com' }
   # Show full error reports.
   config.consider_all_requests_local = true
 
